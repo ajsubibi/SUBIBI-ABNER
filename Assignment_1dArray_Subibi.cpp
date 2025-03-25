30e7da4ab66c18b0fab613aa81aa669db29c53df
ajsubibi <ae202402650@wmsu.edu.ph>
2025-03-25 23:13:22 +0800

    Update Assignment_1dArray_Subibi.cpp

--- Assignment_1dArray_Subibi.cpp
+++ Assignment_1dArray_Subibi.cpp
@@ -12,7 +12,7 @@ struct Student {
     int Id;
     string FirstName;
     string LastName;
-    string Course;
+    string Section;
     float Gpa;
 };
 
@@ -23,8 +23,7 @@ int FindStudentById(int StudentId);
 void AddStudent();
 void EditStudent();
 void DeleteStudent();
-void DisplayStudent();
-void DisplayAllStudents();
+void ViewStudents();
 void ShowMenu();
 
 int main() {
@@ -48,20 +47,22 @@ void AddStudent() {
     }
 
     Student NewStudent;
-    cout << "Enter student ID: ";
-    cin >> NewStudent.Id;
+    
+    do {
+        cout << "Enter student ID: ";
+        cin >> NewStudent.Id;
 
-    if (FindStudentById(NewStudent.Id) != -1) {
-        cout << "Student ID already exists." << endl;
-        return;
-    }
+        if (FindStudentById(NewStudent.Id) != -1) {
+            cout << "Student ID already exists. Please enter a different ID." << endl;
+        }
+    } while (FindStudentById(NewStudent.Id) != -1);
 
     cout << "Enter first name: ";
     cin >> NewStudent.FirstName;
     cout << "Enter last name: ";
     cin >> NewStudent.LastName;
-    cout << "Enter course: ";
-    cin >> NewStudent.Course;
+    cout << "Enter section: ";
+    cin >> NewStudent.Section;
     cout << "Enter GPA: ";
     cin >> NewStudent.Gpa;
 
@@ -86,8 +87,8 @@ void EditStudent() {
     cin >> StudentList[Index].FirstName;
     cout << "Enter new last name: ";
     cin >> StudentList[Index].LastName;
-    cout << "Enter new course: ";
-    cin >> StudentList[Index].Course;
+    cout << "Enter new section: ";
+    cin >> StudentList[Index].Section;
     cout << "Enter new GPA: ";
     cin >> StudentList[Index].Gpa;
 
@@ -95,6 +96,11 @@ void EditStudent() {
 }
 
 void DeleteStudent() {
+    if (StudentCount == 0) {
+        cout << "No student records found." << endl;
+        return;
+    }
+
     int StudentId;
     cout << "Enter student ID to delete: ";
     cin >> StudentId;
@@ -105,66 +111,80 @@ void DeleteStudent() {
         return;
     }
 
-    for (int i = Index; i < StudentCount - 1; i++) {
-        StudentList[i] = StudentList[i + 1];
-    }
-    StudentCount--;
+    cout << "Student found: " << StudentList[Index].FirstName << " " << StudentList[Index].LastName << endl;
+    cout << "Are you sure you want to delete this student? (1 - Yes, 2 - No): ";
+    int choice;
+    cin >> choice;
 
-    cout << "Student deleted successfully." << endl;
+    if (choice == 1) {
+        for (int i = Index; i < StudentCount - 1; i++) {
+            StudentList[i] = StudentList[i + 1];
+        }
+        StudentCount--;
+        cout << "Student deleted successfully." << endl;
+    } else {
+        cout << "Deletion canceled." << endl;
+    }
 }
 
-void DisplayStudent() {
-    int StudentId;
-    cout << "Enter student ID to search: ";
-    cin >> StudentId;
-
-    int Index = FindStudentById(StudentId);
-    if (Index == -1) {
-        cout << "Student not found." << endl;
-        return;
-    }
-
-    cout << "ID: " << StudentList[Index].Id << endl
-         << "Name: " << StudentList[Index].FirstName << " " << StudentList[Index].LastName << endl
-         << "Course: " << StudentList[Index].Course << endl
-         << "GPA: " << StudentList[Index].Gpa << endl;
-}
-
-void DisplayAllStudents() {
+void ViewStudents() {
     if (StudentCount == 0) {
         cout << "No students found." << endl;
         return;
     }
 
-    for (int i = 0; i < StudentCount - 1; i++) {
-        for (int j = 0; j < StudentCount - i - 1; j++) {
-            if (StudentList[j].Gpa < StudentList[j + 1].Gpa) {
-                swap(StudentList[j], StudentList[j + 1]);
-            }
-        }
+    int option;
+    cout << "Choose viewing option:" << endl;
+    cout << "1. Alphabetical Order" << endl;
+    cout << "2. GPA (Ascending Order)" << endl;
+    cout << "Enter choice: ";
+    cin >> option;
+
+    Student SortedList[MaxStudents];
+    for (int i = 0; i < StudentCount; i++) {
+        SortedList[i] = StudentList[i];
     }
 
-    cout << endl << "Students sorted by GPA:" << endl;
+    if (option == 1) {
+        for (int i = 0; i < StudentCount - 1; i++) {
+            for (int j = 0; j < StudentCount - i - 1; j++) {
+                if (SortedList[j].FirstName > SortedList[j + 1].FirstName) {
+                    swap(SortedList[j], SortedList[j + 1]);
+                }
+            }
+        }
+        cout << endl << "Students Sorted Alphabetically:" << endl;
+    } else if (option == 2) {
+        for (int i = 0; i < StudentCount - 1; i++) {
+            for (int j = 0; j < StudentCount - i - 1; j++) {
+                if (SortedList[j].Gpa > SortedList[j + 1].Gpa) {
+                    swap(SortedList[j], SortedList[j + 1]);
+                }
+            }
+        }
+        cout << endl << "Students Sorted by GPA (Ascending Order):" << endl;
+    } else {
+        cout << "Invalid choice." << endl;
+        return;
+    }
+
     for (int i = 0; i < StudentCount; i++) {
-        cout << StudentList[i].Id << " - " << StudentList[i].FirstName << " " << StudentList[i].LastName
-             << " - " << StudentList[i].Course << " - GPA: " << StudentList[i].Gpa << endl;
+        cout << SortedList[i].Id << " - " << SortedList[i].FirstName << " " << SortedList[i].LastName
+             << " - " << SortedList[i].Section << " - GPA: " << SortedList[i].Gpa << endl;
     }
 }
 
 void ShowMenu() {
     int Choice;
     do {
-        cout << endl
-              << "   STUDENT MANAGEMENT MENU   " << endl
-             
-             << "1. Add New Student" << endl
-             << "2. Edit Student Information" << endl
-             << "3. Remove Student" << endl
-             << "4. Search for a Student" << endl
-             << "5. View All Students" << endl
-             << "6. Exit Program" << endl
-             << "----------------------------" << endl
-             << "Enter your choice: ";
+        cout << endl << "   STUDENT MANAGEMENT MENU   " << endl;
+        cout << "1. Add Student" << endl;
+        cout << "2. Edit Student" << endl;
+        cout << "3. Delete Student" << endl;
+        cout << "4. View Students" << endl;
+        cout << "5. Exit" << endl;
+        cout << "----------------------------" << endl;
+        cout << "Enter your choice: ";
         cin >> Choice;
 
         switch (Choice) {
@@ -178,76 +198,13 @@ void ShowMenu() {
                 DeleteStudent();
                 break;
             case 4:
-                DisplayStudent();
+                ViewStudents();
                 break;
             case 5:
-                DisplayAllStudents();
-                break;
-            case 6:
                 cout << "Exiting program... Goodbye!" << endl;
-                break;
+                return;
             default:
                 cout << "Invalid choice. Please try again." << endl;
         }
-    } while (Choice != 6);
-}
-        cout << "Student not found" << endl;
-        return;
-    }
-
-    cout << "ID: " << students[index].student_id << endl
-         << "Name: " << students[index].first_name << " " << students[index].last_name << endl
-         << "Course: " << students[index].course << endl
-         << "GPA: " << students[index].gpa << endl;
-}
-
-void display_all_students() {
-    if (student_count == 0) {
-        cout << "No students found" << endl;
-        return;
-    }
-
-    for (int i = 0; i < student_count - 1; i++) {
-        for (int j = 0; j < student_count - i - 1; j++) {
-            if (students[j].gpa < students[j + 1].gpa) {
-                swap(students[j], students[j + 1]);
-            }
-        }
-    }
-
-    cout << "Students sorted by GPA:" << endl;
-    for (int i = 0; i < student_count; i++) {
-        cout << students[i].student_id << " - " << students[i].first_name << " " << students[i].last_name
-             << " - " << students[i].course << " - GPA: " << students[i].gpa << endl;
-    }
-}
-
-void menu() {
-    int choice;
-    while (true) {
-        cout << endl
-             << "1. Add Student" << endl
-             << "2. Edit Student" << endl
-             << "3. Delete Student" << endl
-             << "4. Search Student" << endl
-             << "5. Show All Students" << endl
-             << "6. Exit" << endl
-             << "Choice: ";
-        cin >> choice;
-
-        switch (choice) {
-            case 1: add_student(); break;
-            case 2: edit_student(); break;
-            case 3: delete_student(); break;
-            case 4: display_student(); break;
-            case 5: display_all_students(); break;
-            case 6: cout << "Exiting" << endl; return;
-            default: cout << "Invalid choice" << endl;
-        }
-    }
-}
-
-int main() {
-    menu();
-    return 0;
-}
+    } while (true);
+}     
